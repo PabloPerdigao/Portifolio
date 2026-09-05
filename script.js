bba075c892e49845fc32771d02e96db5b4741171
PabloPerdigao <pabloagps@gmail.com>
2026-09-05 11:20:14 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,69 @@
+// --- 1. EFEITO MATRIX RAIN (Fundo da Hero) ---
+const canvas = document.getElementById("matrixCanvas");
+const ctx = canvas.getContext("2d");
+
+function resizeCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+window.addEventListener("resize", resizeCanvas);
+resizeCanvas();
+
+const characters = "PABLO01FRONTEND<>{}0101CODE";
+const fontSize = 14;
+const columns = canvas.width / fontSize;
+const drops = [];
+
+for (let x = 0; x < columns; x++) {
+  drops[x] = 1;
+}
+
+function drawMatrix() {
+  // Fundo semi-transparente para criar o rastro
+  ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  ctx.fillStyle = "#20c997"; // Cor Teal
+  ctx.font = fontSize + "px monospace";
+
+  for (let i = 0; i < drops.length; i++) {
+    const text = characters.charAt(
+      Math.floor(Math.random() * characters.length),
+    );
+    ctx.fillText(text, i * fontSize, drops[i] * fontSize);
+
+    if (drops[i] * fontSize > canvas.height && Math.random() > 0.975) {
+      drops[i] = 0;
+    }
+    drops[i]++;
+  }
+}
+setInterval(drawMatrix, 35); // Velocidade da chuva
+
+// --- 2. LÓGICA ASSINATURA MOVIMENTO NOME (Parallax Imagem) ---
+
+document.addEventListener("mousemove", (e) => {
+    const moveX = e.clientX * -0.015;
+    const moveY = e.clientY * -0.015;
+    
+    const heroTitle = document.querySelector(".hero-content h1");
+    if (heroTitle) {
+        heroTitle.style.transform = `translate(${moveX}px, ${moveY}px)`;
+    }
+    
+});
+
+
+// --- 3. LÓGICA ASSINATURA PERMANECER VISÍVEL ( ao entrar no elemetno) ---
+const signatureContainer = document.getElementById("signature-wrapper");
+
+if(signatureContainer) {
+    signatureContainer.addEventListener('mouseenter', () => {
+        signatureContainer.classList.add('revelado');
+    }, {once: true});
+
+    if(hintText) {
+    hintText.style.opacity = '0';
+    hintText.style.display = 'none';
+}
+}
